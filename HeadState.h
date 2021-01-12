59d529991f8d5cc14e993e37c811728791e4554e
Sang <tsangz189@gmail.com>
2021-01-13 00:40:31 +0800

    Initialize

--- HeadState.h
+++ HeadState.h
@@ -1,10 +1,16 @@
 #ifndef HEAD_STATE_H_
 #define HEAD_STATE_H_
 
+#include "HeadGesture.h"
+
 class HeadGesture;
 
 class HeadState {
+ public:
+  virtual void enter(HeadGesture* gesture) = 0;
+  virtual void exit(HeadGesture* gesture) = 0;
 
+  virtual ~HeadState() {}
 };
 
-#endif // HEAD_STATE_H_
+#endif  // HEAD_STATE_H_
